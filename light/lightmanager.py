1e6da557e0bfa8d70af05bf271c30dfedd07abe0
chrisjtwomey <chrisjamestwomey@gmail.com>
2021-10-30 12:46:31 +0100

    Less querying off lifx devices

--- light/lightmanager.py
+++ light/lightmanager.py
@@ -1,4 +1,5 @@
 import lifxlan
+from lifxlan.device import Device
 import pytz
 import math
 import time
@@ -12,8 +13,8 @@ from astral.sun import sun
 class LightManager:
     CORNER_LAMP_GROUP_NAME = "LIGHTMANAGER_DEVICE_GROUP_CORNER_LAMP"
     DESK_LAMP_GROUP_NAME = "LIGHTMANAGER_DEVICE_GROUP_DESK_LAMP"
-    STATIC_LIGHT_REFRESH_TIMEOUT_SECONDS = 120
-    DEVICES_OFF_TIMEOUT_SECONDS = 60
+    CACHE_REFRESH_TIMEOUT_SECONDS = 120
+    DEVICES_OFF_TIMEOUT_SECONDS = 120
 
     GEO_DEFAULT_CITY = "Dublin"
     GEO_DEFAULT_TZ = "UTC"
@@ -35,15 +36,23 @@ class LightManager:
 
         self._light_hours = managerconf.getint("static_light_hours")
 
-
         nowtime_naive = time.time()
-        self._pir_detection_time = nowtime_naive
         self._power_transition_time = nowtime_naive
         self._static_lights_refresh_time = 0
-        self._prev_livebody_detection = True # init PIR lights as on
+        self._prev_livebody_detection = True  # init PIR lights as on
 
         self._devicegroups = self.get_devicegroups()
-        self._devicestates = dict()
+        self._state_cache = self._init_state_cache(self._devicegroups)
+        self.get_devices_power_state()
+
+    def _init_state_cache(self, devicegroups):
+        cache = dict()
+
+        for _, group in devicegroups.items():
+            for device in group.get_device_list():
+                cache[device.mac_addr] = (0, False)
+
+        return cache
 
     def get_devicegroups(self):
         discovery_enabled = self.discoveryconf.getboolean("enabled")
@@ -92,80 +101,71 @@ class LightManager:
         # controlled by PIR motion sensor
         pir_controlled_light_groups = []
 
-        try:
-            if nowdate_tzaware < static_lights_on_dt:
-                # between midnight and dawn
-                # static lights stay off
-                # ambient lights activated by PIR
-                static_light_groups = self.GROUPS_PLANT
-                pir_controlled_light_groups = self.GROUPS_AMBIENT
+        if nowdate_tzaware < static_lights_on_dt:
+            # between midnight and dawn
+            # static lights stay off
+            # ambient lights activated by PIR
+            static_light_groups = self.GROUPS_PLANT
+            pir_controlled_light_groups = self.GROUPS_AMBIENT
 
-                states = self.get_devices_power_state(static_light_groups)
+            states = self.get_devices_power_state(static_light_groups)
+            if any(states):
+                # keep static lights off
+                self.set_devices_power_state(static_light_groups, False)
+
+        elif static_lights_off_dt < nowdate_tzaware < midnight_tmrw:
+            # between static lights off and midnight
+            # ambient and plant lights activated by PIR
+            static_light_groups = []
+            pir_controlled_light_groups = self.GROUPS_AMBIENT + self.GROUPS_PLANT
+
+        elif static_lights_on_dt < nowdate_tzaware < static_lights_off_dt:
+            # between static lights on and static lights off
+            # static lights stay on
+            # ambient lights activated by PIR
+            static_light_groups = self.GROUPS_PLANT
+            pir_controlled_light_groups = self.GROUPS_AMBIENT
+
+            states = self.get_devices_power_state(static_light_groups)
+            if not all(states):
+                # keep static lights on
+                self.set_devices_power_state(static_light_groups, True)
+
+        # PIR motion detected
+        if livebody_detection:
+            states = self.get_devices_power_state(
+                pir_controlled_light_groups)
+            if not all(states):
+                # set all devices on
+                self.set_devices_power_state(
+                    pir_controlled_light_groups, True)
+                self._power_transition_time = nowtime_naive
+
+        if not livebody_detection:
+            # are we on longer than we should without detecting anybody?
+            time_since_transition = math.ceil(
+                nowtime_naive - self._power_transition_time)
+
+            if time_since_transition >= self.DEVICES_OFF_TIMEOUT_SECONDS:
+                states = self.get_devices_power_state(
+                    pir_controlled_light_groups)
+                # are any on?
                 if any(states):
-                    # keep static lights off
-                    self.set_devices_power_state(static_light_groups, False)
+                    self.log.debug("No PIR motion detected for {} seconds for groups {}".format(
+                        self.DEVICES_OFF_TIMEOUT_SECONDS, pir_controlled_light_groups))
+                    # set all devices off
+                    self.set_devices_power_state(
+                        pir_controlled_light_groups, False)
+                    self._power_transition_time = nowtime_naive
 
-            elif static_lights_off_dt < nowdate_tzaware < midnight_tmrw:
-                # between static lights off and midnight
-                # ambient and plant lights activated by PIR
-                static_light_groups = []
-                pir_controlled_light_groups = self.GROUPS_AMBIENT + self.GROUPS_PLANT
+    def update_state_cache(self, device, power_state):
+        nowtime_naive = time.time()
+        self._state_cache[device.mac_addr] = (nowtime_naive, power_state)
 
-            elif static_lights_on_dt < nowdate_tzaware < static_lights_off_dt:
-                # between static lights on and static lights off
-                # static lights stay on
-                # ambient lights activated by PIR
-                static_light_groups = self.GROUPS_PLANT
-                pir_controlled_light_groups = self.GROUPS_AMBIENT
+    def get_devices_power_state(self, groups=[], use_local_state=True):
+        if len(groups) == 0:
+            groups = self.GROUPS_AMBIENT + self.GROUPS_PLANT
 
-                states = self.get_devices_power_state(static_light_groups)
-                if not all(states):
-                    # keep static lights on
-                    self.set_devices_power_state(static_light_groups, True)
-
-            # PIR motion detected
-            if livebody_detection != self._prev_livebody_detection:
-                if livebody_detection:
-                    self._pir_detection_time = nowtime_naive
-
-                    states = self.get_devices_power_state(
-                        pir_controlled_light_groups, use_local_state=False)
-                    if not all(states):
-                        # set all devices on
-                        self.set_devices_power_state(
-                            pir_controlled_light_groups, True)
-                        self._power_transition_time = nowtime_naive
-                        
-                    self._prev_livebody_detection = livebody_detection
-
-                if not livebody_detection:
-                    # are we on longer than we should without detecting anybody?
-                    time_since_transition = math.ceil(
-                        nowtime_naive - self._power_transition_time)
-
-                    if time_since_transition >= self.DEVICES_OFF_TIMEOUT_SECONDS:
-                        self.log.debug("Timeout condition hit without PIR motion detection for groups {}".format(pir_controlled_light_groups))
-                        states = self.get_devices_power_state(
-                            pir_controlled_light_groups, use_local_state=False)
-                        # are any on?
-                        if any(states):
-                            # set all devices off
-                            self.set_devices_power_state(
-                                pir_controlled_light_groups, False)
-                            self._power_transition_time = nowtime_naive
-
-                    self._prev_livebody_detection = livebody_detection
-
-        except lifxlan.WorkflowException as wfe:
-            # catch exception here as we don't want to stop on
-            # communication errors
-            self.log.error('Error occurred communicating with LIFX lights')
-            self.log.exception(wfe)
-        except Exception as e:
-            self.log.error('A generic exception error has occurred')
-            self.log.exception(e)
-
-    def get_devices_power_state(self, groups, use_local_state=True):
         devices = []
         for group in groups:
             group = self._devicegroups[group]
@@ -174,29 +174,50 @@ class LightManager:
                 devices.append(device)
 
         nowtime_naive = time.time()
-        time_since_update = math.ceil(nowtime_naive - self._static_lights_refresh_time)
-
         device_power_states = []
-        if use_local_state and time_since_update < self.STATIC_LIGHT_REFRESH_TIMEOUT_SECONDS:
-            for device in devices:
-                self.log.debug("Getting power state for device {}:{} from cache".format(device.mac_addr, device.ip_addr))
-                device_state = self._devicestates[device.mac_addr]
-                device_power_states.append(device_state)
-        else:
-            for device in devices:
-                self.log.debug("Getting power state for device {}:{}".format(device.mac_addr, device.ip_addr))
-                device_state = device.get_power() > 0
-                self._devicestates[device.mac_addr] = device_state
-                device_power_states.append(device_state)
 
-            self._static_lights_refresh_time = nowtime_naive
+        for device in devices:
+            refresh_time, power_state = self._state_cache[device.mac_addr]
+            time_since_update = math.ceil(nowtime_naive - refresh_time)
 
-        self.log.debug("LIFX device power states: {}".format(device_power_states))
+            if not use_local_state or time_since_update > self.CACHE_REFRESH_TIMEOUT_SECONDS:
+                self.log.info("Querying device {}:{} for power state...".format(
+                    device.mac_addr, device.ip_addr))
+                try:
+                    power_state = device.get_power() > 0
+                    self.update_state_cache(device, power_state)
+                except lifxlan.WorkflowException as wfe:
+                    # catch exception here as we don't want to stop on
+                    # communication errors
+                    self.log.error(
+                        'Error occurred communicating with LIFX lights')
+                    self.log.exception(wfe)
+                except Exception as e:
+                    self.log.error('A generic exception error has occurred')
+                    self.log.exception(e)
+
+            device_power_states.append(power_state)
+
+        self.log.debug("LIFX device power states for groups {}: {}".format(
+            ", ".join(groups), device_power_states))
 
         return device_power_states
 
     def set_devices_power_state(self, groups, state):
         for group in groups:
-            self.log.debug("Setting power state {} for LIFX device grous: {}".format(state, group))
+            self.log.debug(
+                "Setting power state {} for LIFX device groups: {}".format(state, group))
             g = self._devicegroups[group]
             g.set_power(state)
+            try:
+                g.set_power(state)
+                for device in g.get_device_list():
+                    self.update_state_cache(device, state)
+            except lifxlan.WorkflowException as wfe:
+                # catch exception here as we don't want to stop on
+                # communication errors
+                self.log.error('Error occurred communicating with LIFX lights')
+                self.log.exception(wfe)
+            except Exception as e:
+                self.log.error('A generic exception error has occurred')
+                self.log.exception(e)
